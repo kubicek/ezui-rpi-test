6a6d14c7ead2eed6ec5860e130246b3d840f095f
Jiří Kubíček <jiri.kubicek@kraxnet.cz>
2024-11-16 03:24:40 +0100

    tmp easy

--- stage-ezui/01-install-packages/02-run.sh
+++ stage-ezui/01-install-packages/02-run.sh
@@ -1,12 +1,3 @@
 on_chroot << EOF
   raspi-config nonint do_boot_behaviour B2
-  systemctl enable nodered.service
-  plymouth-set-default-theme --rebuild-initrd spinfinity
-
-  echo "if [[ -z \$SSH_CONNECTION ]]; then
-    startx  -- -nocursor /usr/bin/chromium-browser --noerrdialogs --disable-infobars --kiosk http://localhost:1880/ui
-fi" >> /home/ezui/.bashrc
-
-  su ezui -c "bash <(curl -sL https://raw.githubusercontent.com/node-red/linux-installers/master/deb/update-nodejs-and-nodered)"
-  su ezui -c "~/.node-red; npm i node-red-dashboard"
 EOF
