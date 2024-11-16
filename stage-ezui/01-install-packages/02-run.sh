07baa2f493b3b0cedd24cd99657cc2cfb9de0a5a
Jiří Kubíček <jiri.kubicek@kraxnet.cz>
2024-11-16 02:58:44 +0100

    chmod

--- stage-ezui/01-install-packages/02-run.sh
+++ stage-ezui/01-install-packages/02-run.sh
@@ -6,4 +6,7 @@ on_chroot << EOF
   echo "if [[ -z \$SSH_CONNECTION ]]; then
     startx  -- -nocursor /usr/bin/chromium-browser --noerrdialogs --disable-infobars --kiosk http://localhost:1880/ui
 fi" >> /home/ezui/.bashrc
+
+  su ezui -c "bash <(curl -sL https://raw.githubusercontent.com/node-red/linux-installers/master/deb/update-nodejs-and-nodered)"
+  su ezui -c "~/.node-red; npm i node-red-dashboard"
 EOF
