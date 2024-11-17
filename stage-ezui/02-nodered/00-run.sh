45ab95f8483321acc9c685a361690457fff4b129
Jiří Kubíček <jiri.kubicek@kraxnet.cz>
2024-11-17 03:11:29 +0100

    nodered try fix

---
+++ stage-ezui/02-nodered/00-run.sh
@@ -0,0 +1,11 @@
+#!/bin/bash -e
+
+on_chroot << EOF
+  cd "/home/${FIRST_USER_NAME}/"
+  su "${FIRST_USER_NAME}" -c "bash <(curl -sL https://raw.githubusercontent.com/node-red/linux-installers/master/deb/update-nodejs-and-nodered) --confirm-install --confirm-pi --no-init"
+
+  cd "/home/${FIRST_USER_NAME}/.node-red"
+  su "${FIRST_USER_NAME}" -c "npm i node-red-dashboard"
+
+  systemctl enable nodered.service
+EOF
