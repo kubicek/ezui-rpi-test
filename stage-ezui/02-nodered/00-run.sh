56ec006c8e78da6b44d2c6791edf98ba4ea3de26
Jiří Kubíček <jiri.kubicek@kraxnet.cz>
2024-11-17 04:37:52 +0100

    pkg preinstall

--- stage-ezui/02-nodered/00-run.sh
+++ stage-ezui/02-nodered/00-run.sh
@@ -2,7 +2,7 @@
 
 on_chroot << EOF
   cd "/home/${FIRST_USER_NAME}/"
-  su "${FIRST_USER_NAME}" -c "bash <(curl -sL https://raw.githubusercontent.com/node-red/linux-installers/master/deb/update-nodejs-and-nodered) --confirm-install --confirm-pi --no-init"
+  su "${FIRST_USER_NAME}" -c "bash <(curl -sL https://raw.githubusercontent.com/node-red/linux-installers/master/deb/update-nodejs-and-nodered) --confirm-install --confirm-pi"
 
   cd "/home/${FIRST_USER_NAME}/.node-red"
   su "${FIRST_USER_NAME}" -c "npm i node-red-dashboard"
