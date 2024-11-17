9f19d90c1b3c3ffaa4d55b064711b42fa343aa52
Jiří Kubíček <jiri.kubicek@kraxnet.cz>
2024-11-17 05:15:40 +0100

    try sudo

--- stage-ezui/02-nodered/00-run.sh
+++ stage-ezui/02-nodered/00-run.sh
@@ -2,10 +2,10 @@
 
 on_chroot << EOF
   cd "/home/${FIRST_USER_NAME}/"
-  su "${FIRST_USER_NAME}" -c "bash <(curl -sL https://raw.githubusercontent.com/node-red/linux-installers/master/deb/update-nodejs-and-nodered) --confirm-install --confirm-pi"
+  sudo -u "${FIRST_USER_NAME}" -c "bash <(curl -sL https://raw.githubusercontent.com/node-red/linux-installers/master/deb/update-nodejs-and-nodered) --confirm-install --confirm-pi"
 
   cd "/home/${FIRST_USER_NAME}/.node-red"
-  su "${FIRST_USER_NAME}" -c "npm i node-red-dashboard"
+  sudo -u "${FIRST_USER_NAME}" -c "npm i node-red-dashboard"
 
   systemctl enable nodered.service
 EOF
