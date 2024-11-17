93019fdff47e8704cd116f9ea7cb71cf63bde293
Jiří Kubíček <jiri.kubicek@kraxnet.cz>
2024-11-17 14:53:11 +0100

    nodered new

--- stage-ezui/02-nodered/00-run.sh
+++ stage-ezui/02-nodered/00-run.sh
@@ -1,26 +1,4 @@
 #!/bin/bash -e
 
-# from https://github.com/PionixInternal/pi-gen/blob/master/stage2/11-install-nodered/01-run.sh
-
-on_chroot << EOF
-  echo -------------------------------------------------------------------------
-  cat /etc/resolv.conf
-  echo -------------------------------------------------------------------------
-  echo nameserver 8.8.8.8 > /tmp/dhcpcd.resolv.conf
-  echo 2-------------------------------------------------------------------------
-  cat /etc/resolv.conf
-  echo 2-------------------------------------------------------------------------
-
-  cd "/home/${FIRST_USER_NAME}/"
-  curl -sL https://raw.githubusercontent.com/node-red/linux-installers/master/deb/update-nodejs-and-nodered -o nodered.sh
-  chmod a+x nodered.sh
-  sudo -u "${FIRST_USER_NAME}" "./nodered.sh --confirm-install --confirm-pi"
-
-  # cd "/home/${FIRST_USER_NAME}/"
-  # sudo -u "${FIRST_USER_NAME}" "bash <(curl -sL https://raw.githubusercontent.com/node-red/linux-installers/master/deb/update-nodejs-and-nodered) --confirm-install --confirm-pi"
-
-  # cd "/home/${FIRST_USER_NAME}/.node-red"
-  # sudo -u "${FIRST_USER_NAME}" "npm i node-red-dashboard"
-
-  systemctl enable nodered.service
-EOF
+mkdir -p ${ROOTFS_DIR}/home/${FIRST_USER_NAME}/.node-red
+mkdir -p ${ROOTFS_DIR}/home/${FIRST_USER_NAME}/.npm
