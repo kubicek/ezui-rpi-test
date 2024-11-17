1ca6934e8165ed71a0f445498a60aa6cf77f1e43
Jiří Kubíček <jiri.kubicek@kraxnet.cz>
2024-11-17 19:16:13 +0100

    startx

--- stage-ezui/03-splash-auto-boot/00-run-chroot.sh
+++ stage-ezui/03-splash-auto-boot/00-run-chroot.sh
@@ -2,5 +2,6 @@ raspi-config nonint do_boot_behaviour B2
 plymouth-set-default-theme --rebuild-initrd spinfinity
   
 echo "if [[ -z \$SSH_CONNECTION ]]; then
-  startx  -- -nocursor /usr/bin/chromium-browser --noerrdialogs --disable-infobars --kiosk http://localhost:1880/ui
+  startx /usr/bin/chromium-browser --noerrdialogs --disable-infobars --kiosk http://localhost:1880/ui -- -nocursor > /dev/null 2>&1
 fi" >> /home/${FIRST_USER_NAME}/.bashrc
+
