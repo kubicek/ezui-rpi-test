0ef73607789273fbea8f096602193b2bbd0ff223
Jiří Kubíček <jiri.kubicek@kraxnet.cz>
2024-11-17 10:32:49 +0100

    netfix

--- stage-ezui/02-nodered/00-run.sh
+++ stage-ezui/02-nodered/00-run.sh
@@ -1,6 +1,14 @@
 #!/bin/bash -e
 
 on_chroot << EOF
+  echo -------------------------------------------------------------------------
+  cat /etc/resolv.conf
+  echo -------------------------------------------------------------------------
+  echo nameserver 8.8.8.8 > /tmp/dhcpcd.resolv.conf
+  echo 2-------------------------------------------------------------------------
+  cat /etc/resolv.conf
+  echo 2-------------------------------------------------------------------------
+
   cd "/home/${FIRST_USER_NAME}/"
   sudo -u "${FIRST_USER_NAME}" "bash <(curl -sL https://raw.githubusercontent.com/node-red/linux-installers/master/deb/update-nodejs-and-nodered) --confirm-install --confirm-pi"
 
