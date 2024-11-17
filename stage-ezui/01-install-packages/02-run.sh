ab91ecb1e8beb3546550d3d3bb7c3899515d611c
Jiří Kubíček <jiri.kubicek@kraxnet.cz>
2024-11-17 01:31:53 +0100

    finishing fix

--- stage-ezui/01-install-packages/02-run.sh
+++ stage-ezui/01-install-packages/02-run.sh
@@ -2,8 +2,8 @@ on_chroot << EOF
   raspi-config nonint do_boot_behaviour B2
   plymouth-set-default-theme --rebuild-initrd spinfinity
 
-  SUDO_USER="${FIRST_USER_NAME}" bash <(curl -sL https://raw.githubusercontent.com/node-red/linux-installers/master/deb/update-nodejs-and-nodered)
-  SUDO_USER="${FIRST_USER_NAME}" ~/.node-red; npm i node-red-dashboard
+  bash <(curl -sL https://raw.githubusercontent.com/node-red/linux-installers/master/deb/update-nodejs-and-nodered)
+  cd ~/.node-red && npm i node-red-dashboard
   systemctl enable nodered.service
   
   echo "if [[ -z \$SSH_CONNECTION ]]; then
