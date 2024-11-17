c97c666f293ef03ec9c37d5ba5da390313372c92
Jiří Kubíček <jiri.kubicek@kraxnet.cz>
2024-11-17 15:21:21 +0100

    last fix

--- stage-ezui/03-splash-auto-boot/00-run-chroot.sh
+++ stage-ezui/03-splash-auto-boot/00-run-chroot.sh
@@ -4,4 +4,3 @@ plymouth-set-default-theme --rebuild-initrd spinfinity
 echo "if [[ -z \$SSH_CONNECTION ]]; then
   startx  -- -nocursor /usr/bin/chromium-browser --noerrdialogs --disable-infobars --kiosk http://localhost:1880/ui
 fi" >> /home/${FIRST_USER_NAME}/.bashrc
-EOF
